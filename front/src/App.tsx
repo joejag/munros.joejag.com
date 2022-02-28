3a04f2cd3d7b0e95d22070fb92a670573014602f
joe.wright <joe@joejag.com>
2022-02-28 16:39:54 +0000

    lomond style

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,23 +1,68 @@
 import React from 'react'
-import './App.css'
 import { MUNROS } from './munros'
 
-const LOMOND = MUNROS['lochlomond_ben_lomond']
+import Grid from '@mui/material/Grid'
+import Container from '@mui/material/Container'
 
-function App() {
-  const munros = LOMOND.munros.length
-  const distance = LOMOND.distance
-  const ascent = LOMOND.ascent
-  const nai = LOMOND.time.naismith
-  const grade = LOMOND.grade
-  const img = LOMOND.image
-  const name = LOMOND.title
-  const desc = LOMOND.desc
+import Card from '@mui/material/Card'
+import CardActions from '@mui/material/CardActions'
+import CardContent from '@mui/material/CardContent'
+import CardMedia from '@mui/material/CardMedia'
+import Typography from '@mui/material/Typography'
+import Divider from '@mui/material/Divider'
 
+import Badge from '@mui/material/Badge'
+import FilterHdrIcon from '@mui/icons-material/FilterHdr'
+
+const App = () => {
+  const munros = Object.values(MUNROS).filter(
+    (m: any) => m.location.steveFallon.area === 'Loch Lomond to Loch Tay'
+  )
   return (
-    <main>
-      {name} : {munros}
-    </main>
+    <Container maxWidth="xl" component="main">
+      <Typography variant="h3">Loch Lomond to Loch Tay</Typography>
+      <Grid container spacing={0}>
+        {munros.map((trip) => {
+          return <TripCard trip={trip} key={trip.title}></TripCard>
+        })}
+      </Grid>
+    </Container>
+  )
+}
+
+interface Trip {
+  title: string
+  desc: string
+  image: string
+  grade: number
+  url: string
+  munros: any[]
+  distance: number
+  ascent: number
+  time: any
+}
+
+const TripCard: React.FC<{ trip: Trip }> = ({ trip }) => {
+  return (
+    <Card sx={{ maxWidth: 345 }}>
+      <CardMedia component="img" height="140" image={trip.image} />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          {trip.title}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          {trip.desc}
+        </Typography>
+      </CardContent>
+      <Divider />
+      <CardActions>
+        <Badge badgeContent={trip.munros.length} color="primary">
+          <FilterHdrIcon />
+        </Badge>
+        | {trip.distance}k | {trip.ascent}m | {trip.time.naismith} |{' '}
+        {trip.grade}{' '}
+      </CardActions>
+    </Card>
   )
 }
 
